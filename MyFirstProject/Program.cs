835b1f4cd45b5b8099f1d840fceb44fbe2d6bbfa
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-07-05 18:56:40 +0800

    Text based data type

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -10,40 +10,14 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            //int x;
-            //int y;
-            //int z; 
+            string name = "Aba";
+            char letter = 'a';
 
-            int x, y, z; 
+            Console.Write("Your name is ");
+            Console.Write(name);
 
-
-            int age = 23; 
-            Console.WriteLine(age);
-            Console.WriteLine(int.MaxValue);
-            Console.WriteLine(int.MinValue);
-
-            long bigNumber = -900000000L;
-            Console.WriteLine(bigNumber);
-            Console.WriteLine(long.MaxValue);
-            Console.WriteLine(long.MinValue);
-
-            double negative = -55.2D;
-            Console.WriteLine(negative);
-            Console.WriteLine(double.MaxValue);
-            Console.WriteLine(double.MinValue);
-
-
-            float precision = 5.000001F;
-            Console.WriteLine(precision);
-            Console.WriteLine(float.MaxValue);
-            Console.WriteLine(float.MinValue);
-
-            decimal money = 14.99M;
-            Console.WriteLine(money);
-            Console.WriteLine(decimal.MaxValue);
-            Console.WriteLine(decimal.MinValue);
-
-
+            Console.WriteLine();
+            Console.WriteLine(letter);
             Console.ReadLine();
         }
     }
