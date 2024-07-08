d8dd7d745bb32e6a484999d48a15b6f3a2b104b9
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-07-08 15:32:21 +0800

    Boolean Data types

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -10,26 +10,14 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            string textAge = "-23";
-            
-            int age = Convert.ToInt32(textAge);
-            Console.WriteLine(age);
+            bool value = true;
 
-            string textBignumber = "-900000000";
-            long bigNumber = Convert.ToInt64(textBignumber);
-            Console.WriteLine(bigNumber);
+            bool isMale = true;
+            Console.WriteLine(isMale);
 
-            string textNegative = " -55.2";
-            double negative = Convert.ToDouble(textNegative);
-            Console.WriteLine(negative);
+            isMale = false;
+            Console.WriteLine(isMale);
 
-            string textPrecision = "5.000001";
-            float precision = Convert.ToSingle(textPrecision);
-            Console.WriteLine(precision);
-
-            string textMoney = "14.99";
-            decimal money = Convert.ToDecimal(textMoney);
-            Console.WriteLine(money);
 
             Console.ReadLine();
         }
