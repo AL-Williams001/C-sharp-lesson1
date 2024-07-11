3f2069832f8977207850cb7cbb2082ee34e5a9f6
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-07-11 16:25:11 +0800

    Storing user data

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -10,17 +10,15 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            const int vat = 20;
-            const double percentVAT = vat / 100D;
+          
+            var name = "Aba";
+            var number = "0123456789";
+            var age = 23;
 
-            int balance = 1000;
+            Console.WriteLine(name);
+            Console.WriteLine(number);
+            Console.WriteLine(age);
 
-            Console.WriteLine(balance * (vat/100D));
-            Console.WriteLine(balance * percentVAT);
-
-            const string version = "v1.0";
-
-            Console.WriteLine(vat);
             Console.ReadLine();
         }
     }
