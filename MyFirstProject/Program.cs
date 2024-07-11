4b694528dc55df53ee33bc9a904e41e85b6b16dd
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-07-11 16:45:31 +0800

    Console Input/Output

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -10,15 +10,23 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Hello my name is AL!");
 
-            int num1 = 10;
-            int num2 = 2;
-            int remainder = num1 % num2;
-            Console.WriteLine(remainder);
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            //Console.WriteLine(name);
 
-            num1 = 11;
-            remainder = num1 % num2;
-            Console.WriteLine(remainder);
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(age);
+
+            //Console.Write("Your name is ");
+            //Console.Write(name);
+            //Console.Write(" and your age is ");
+            //Console.Write(age);
+
+            Console.WriteLine();
+            Console.WriteLine("Your name is " + name + " and your age is " +age);
 
             Console.ReadLine();
         }
