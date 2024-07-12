6c66b5392bb50713e66075ccbc2681eb04acfed8
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-07-12 16:56:26 +0800

    IF Statements

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -10,24 +10,71 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello my name is AL!");
 
-            Console.Write("Enter your name: ");
+
+            /*Console.Write("Enter your name: ");
             string name = Console.ReadLine();
-            //Console.WriteLine(name);
+
 
             Console.Write("Enter your age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine(age);
+            string ageInput = Console.ReadLine();
+            int age = Convert.ToInt32(ageInput);
 
-            //Console.Write("Your name is ");
-            //Console.Write(name);
-            //Console.Write(" and your age is ");
-            //Console.Write(age);
+
 
             Console.WriteLine();
-            Console.WriteLine("Your name is " + name + " and your age is " +age);
+            Console.WriteLine("Your name is " + name + " and your age is " + age);*/
 
+
+            /*if (age >= 18 && age <= 25)
+            {
+                Console.WriteLine("You are between 18 and 25");
+            }
+            else if (age >= 26)
+            {
+                Console.WriteLine("You are 26 and older");
+            }
+
+            if (age < 0 || age > 150)
+            {
+                Console.WriteLine("Inavlid age!");
+            }
+            else
+            {
+                if (age >= 18 && age <= 25)
+                {
+                    Console.WriteLine("You are between 18 and 25");
+                }
+                else if (age >= 26)
+                {
+                    Console.WriteLine("You are 26 and older");
+                }
+                Console.WriteLine("valid age!");
+            }*/
+
+            Console.Write("Enter the first number ");
+            string numberAInput = Console.ReadLine();
+            int numberA = Convert.ToInt32(numberAInput);
+
+             Console.Write("Enter the second number ");
+            string numberBInput = Console.ReadLine();
+            int numberB = Convert.ToInt32(numberBInput);
+
+            int answer = numberA * numberB;
+            Console.Write("Value of " + numberA + "x" + numberB + ": ");
+            string answerInput = Console.ReadLine();
+            int actualAnswer = Convert.ToInt32(answerInput);
+
+            if (answer == actualAnswer)
+            {
+                Console.WriteLine("Well done!");
+            }
+            else 
+            {
+                Console.WriteLine("Close but it is wrong!");
+            }
+
+       
             Console.ReadLine();
         }
     }
