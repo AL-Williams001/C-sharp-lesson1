50638d1d6672dd9154bf200ca46c04de2f94f81c
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-07-23 16:54:04 +0800

    TryParse Function

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -12,28 +12,30 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            double value = 1000D / 12.34;
 
-            Console.WriteLine(value);
-            Console.WriteLine(string.Format("{0:0}", value));
-            Console.WriteLine(string.Format("{0:0.0}", value));
-            Console.WriteLine(string.Format("{0:0.00}", value));
+            bool success = true;
+            while(success)
+            {
 
-            double money = -10D / 3D; //3.333333333
-
-            Console.WriteLine(money);
-            Console.WriteLine(string.Format("$10 / $3 = ${0:0.00}", money));
-            Console.WriteLine(money.ToString("C"));
-            Console.WriteLine(money.ToString("C0"));
-            Console.WriteLine(money.ToString("C1"));
-            Console.WriteLine(money.ToString("C2"));
-
-            Console.WriteLine(money.ToString("C", CultureInfo.CurrentCulture));
-            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-GB")));
-            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
-            Console.WriteLine(money.ToString("C", CultureInfo.CreateSpecificCulture("en-PH")));
+            Console.Write("Enter a number: ");
+            string numInput = Console.ReadLine();
+          
 
 
+            if (int.TryParse(numInput, out int num))
+            {
+                success = false;
+                Console.WriteLine(num);
+            }
+            else
+            {
+                Console.WriteLine("Failed to convert!");
+            }
+
+
+            }
+
+           
 
             Console.ReadLine();
         }
