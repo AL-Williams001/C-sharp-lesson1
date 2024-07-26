25cf11b71d91e72e4386787940f74ff062ffff77
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-07-26 16:55:54 +0800

    FizzBuzz

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -12,12 +12,31 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number: ");
-            int number = Convert.ToInt32(Console.ReadLine());
+            bool threeDiv = false;
+            bool fiveDiv = false;
 
-            for (int i = 1; i <= 10; i++)
+
+            for (int i = 1; i <= 15 ; i++)
             {
-                Console.WriteLine("{0} x {1} = {2}", i, number, i * number);
+                threeDiv = i % 3 == 0;
+                fiveDiv = i % 5 == 0;
+
+                if (threeDiv && fiveDiv)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (threeDiv)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (fiveDiv)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
             }
 
 
