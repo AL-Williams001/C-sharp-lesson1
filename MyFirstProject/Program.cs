cb3935c30759a0f8f13100078dfdec5c78ef7fc9
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-08-14 17:00:09 +0800

    Return type Functions

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -17,38 +17,107 @@ namespace MyFirstProject
 
      class Program
     {
+
+       
         static void Main(string[] args)
         {
 
-            WelcomeMessage();
+            Console.Title = ReturnNameAgePair();
 
-            CreateAndPrintArray();
+            PrintIntroduction();
 
-        
+            int[] numbers = new int[3];
 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                
+            
+            numbers[i] = ReadNumberFromConsole();
 
-            Console.ReadLine();
-        }
-
-
-        static void CreateAndPrintArray()
-        {
-            int[] numbers = new int[5] {0, 1, 2, 3, 4 };
+            }
 
             foreach (var item in numbers)
             {
                 Console.Write($"{item} ");
             }
+
+
+            Console.WriteLine();
+
+            int[] newNumbers = CreateRandomArray();
+
+            foreach (var item in newNumbers)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine(Add());
+
+
+            Console.ReadLine();
         }
 
-
-        static void WelcomeMessage()
+       static void something()
         {
-            Console.WriteLine("Welcome to my program!");
+
+        }
+
+        static void conversion()
+        {
+
+        }
+
+        static void calculation()
+        {
+
+        }
+
+        static int Add()
+        {
+            return 5 + 5; 
         }
 
 
 
+        static int[] CreateRandomArray()
+        {
+            
+
+            return new int[3] { 0,1,2 };
+        }
+
+        static int ReadNumberFromConsole()
+        {
+            Console.Write("Enter a number: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+
+        static string ReturnName()
+        {
+            return "Arthur";
+        }
+
+        static int ReturnAge()
+        {
+            return 23;
+        }
+
+
+        static string ReturnNameAgePair()
+        {
+            return $"{ReturnName()} - {ReturnAge()}";
+        }
+
+        static void PrintIntroduction()
+        {
+            /*string name = ReturnName();
+            int age = ReturnAge();
+            string output = $"Hello my name is {name} and my age is {age}";
+            Console.WriteLine(output);*/
+
+            Console.WriteLine($"Hello my name is {ReturnName()} and my age is {ReturnAge()}");
+        }
 
     }
 }
