5599e7ef3acdd8cd3bb16d3bd40c3789168bd020
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-08-16 16:40:28 +0800

    Optional Parameter

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
@@ -22,59 +23,33 @@ namespace MyFirstProject
         static void Main(string[] args)
         {
 
-            Console.WriteLine(Add(50, 50));
 
-
-            int num = ReadInt("Enter a number");
-            Console.WriteLine(num);
-
-            int angle = ReadInt("Enter a angle");
-            Console.WriteLine(angle);
-
-            int firstNum = ReadInt("Enter first number");
-            int secondNum = ReadInt("Enter second number");
-            int result = Add(firstNum, secondNum);
-
+            int result = Add(5);
             Console.WriteLine(result);
 
-           
-            string name = ReadString("Enter your name");
-            int age = ReadInt("Enter your age");
-            string details = UserDetails(name, age);
-
-            Console.WriteLine(details);
+            PrintName("Art");
 
 
             Console.ReadLine();
 
         }
 
-        static string UserDetails (string name, int age)
+        static void PrintName(string name = "AL") 
         {
-            return $"Hello my name is {name} and my age is {age}!";
+            Console.WriteLine($"My name is {name}");
+
         }
 
-
-        static string ReadString(string message)
-        {
-            Console.WriteLine($"{message}: ");
-            return Console.ReadLine();
+        static int Add(int a, int b = default) 
+        
+        { 
+            return a + b;
         }
 
-        static int ReadInt(string message)
-        {
-            Console.Write($"{message}: ");
-            return Convert.ToInt32(Console.ReadLine());
-        }
-
-
-        static int Add(int a, int b)
-        {
-           return a + b;
-
-        }
 
        
+
+
 
     }
 }
