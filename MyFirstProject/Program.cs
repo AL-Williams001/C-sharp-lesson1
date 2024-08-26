2166296b1d288cd5c9e688861fb05e7e26875f79
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-08-26 16:55:40 +0800

    Debugging

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -24,60 +24,32 @@ namespace MyFirstProject
         {
 
 
+            string name = "AL";
+            int age;
+            age = 23; 
 
-            /*bool success = false;
+            PrintDetails(name, age);
 
-
-
-            try
-            {
-                Console.Write("Enter a number: ");
-                int num = Convert.ToInt32(Console.ReadLine());
-                success = true;
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            Console.WriteLine(success ? "Yey" : "Oh no!");*/
-
-
-            Console.Write("Enter a number: ");
-            if (TryParse(Console.ReadLine(), out int result))
-            {
-                Console.WriteLine("Yey " + result);
-            }
-            else
-            {
-                Console.WriteLine("Oh no!");
-            }
-           
-
-
-
+            string message = ReadFromConsole("Enter anything: ");
+            Console.WriteLine(message);
 
             Console.ReadLine();
 
         }
 
-        static bool TryParse(string input, out int result)
+        static string ReadFromConsole(string message)
         {
-            result = -1;
-
-            try
-            {
-                result = Convert.ToInt32(input);
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-
-            
+            Console.WriteLine(message);
+            return Console.ReadLine();
         }
-       
+
+        static void PrintDetails(string name, int age)
+        {
+            Console.WriteLine($"{name} - {age}");
+            Console.WriteLine($"{name} - {age}");
+            Console.WriteLine($"{name} - {age}");
+            Console.WriteLine($"{name} - {age}");
+        }
 
        
 
