cda64f3957ecdab25eee8a49edbba54b2114cdbc
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-08-30 16:44:06 +0800

    Structures

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -13,40 +13,109 @@ using System.Xml.Linq;
 namespace MyFirstProject
 {
 
-   
 
 
-     class Program
+
+    class Program
     {
-        
-       
+        struct Person
+        {
+            public string name;
+            public int age;
+            public int birthMonth;
+            public int number;
+
+            public Person(string name, int age, int birthMonth, int number)
+            {
+                this.name = name;
+                this.age = age;
+                this.birthMonth = birthMonth;
+                this.number = number;
+
+            }
+        }
+
         static void Main(string[] args)
         {
 
-            int age = 55;
+            /* string name = "AL";
+             int age = 23;
+             int birthMonth = 9;
+
+             Person person;
+
+             person.name = "AL";
+             person.age = 23;
+             person.birthMonth = 9;
+
+             Console.WriteLine($"{name} - {age} - {birthMonth}");
+             Console.WriteLine($"{person.name} - {person.age} - {person.birthMonth}");
 
 
-            if (age > 18 && age < 30) // use conditional to get a specific range
-            {
-                Console.WriteLine("Over 18");
+             string newName = "";
+             int newAge = 0;
+             int newBirthMonth = 0;
+             ReturnPerson(ref newName, ref newAge, ref newBirthMonth);
+             Console.WriteLine($"{newName} - {newAge} - {newBirthMonth}");*/
 
-            }
-            else if(age > 30 && age < 50)
-            {
-                Console.WriteLine("Over 30");
-            }
-            else if (age > 50)
-            {
-                Console.WriteLine("Over 50");
-            }
 
+            Person person = ReturnPerson();
+            Console.WriteLine($"{person.name} - {person.age} - {person.birthMonth} - {person.number}");
 
 
             Console.ReadLine();
 
         }
+
+        static Person ReturnPerson()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your birth month: ");
+            int birthMonth = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+          /*  Person person;
+
+            person.name = name;
+            person.age = age;
+            person.birthMonth = birthMonth;
+            person.number = number*/;
+
+            //return person;
+
+            return new Person(name, age, birthMonth, number);
+        } 
+
+
+
         
-       
+
+
+        /*static void ReturnPerson(ref string name , ref int age, ref int birthMonth)
+        {
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your birth month: ");
+            birthMonth = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+        }*/
+
+
+                    
        
         
 
