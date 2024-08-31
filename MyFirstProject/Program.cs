b49328a6c5c4b8b04d53d057aae293e537f93295
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-08-31 16:23:36 +0800

    Classes

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -18,56 +18,50 @@ namespace MyFirstProject
 
     class Program
     {
-        struct Person
+       /* struct Person
         {
             public string name;
             public int age;
-            public int birthMonth;
-            public int number;
 
-            public Person(string name, int age, int birthMonth, int number)
+            public Person(string name, int age)
             {
                 this.name = name;
                 this.age = age;
-                this.birthMonth = birthMonth;
-                this.number = number;
+            } 
+        }*/
 
+        class Person
+        {
+            public string name;
+            public int age;
+
+            public Person()
+            {
             }
+
+            public Person(string name)
+            {
+                this.name = name;
+                this.age = -1;
+            }
+
+            public Person(int age)
+            {
+                this.age = age;
+                this.name = "";
+            }
+
+
+            public Person(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+
+
         }
 
         static void Main(string[] args)
-        {
-
-            /* string name = "AL";
-             int age = 23;
-             int birthMonth = 9;
-
-             Person person;
-
-             person.name = "AL";
-             person.age = 23;
-             person.birthMonth = 9;
-
-             Console.WriteLine($"{name} - {age} - {birthMonth}");
-             Console.WriteLine($"{person.name} - {person.age} - {person.birthMonth}");
-
-
-             string newName = "";
-             int newAge = 0;
-             int newBirthMonth = 0;
-             ReturnPerson(ref newName, ref newAge, ref newBirthMonth);
-             Console.WriteLine($"{newName} - {newAge} - {newBirthMonth}");*/
-
-
-            Person person = ReturnPerson();
-            Console.WriteLine($"{person.name} - {person.age} - {person.birthMonth} - {person.number}");
-
-
-            Console.ReadLine();
-
-        }
-
-        static Person ReturnPerson()
         {
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
@@ -75,44 +69,16 @@ namespace MyFirstProject
             Console.Write("Enter your age: ");
             int age = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter your birth month: ");
-            int birthMonth = Convert.ToInt32(Console.ReadLine());
+            Person person = new Person(name, age);
 
-            Console.Write("Enter your number: ");
-            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(person.name);
+            Console.WriteLine(person.age);
 
-          /*  Person person;
+            Console.ReadLine();
 
-            person.name = name;
-            person.age = age;
-            person.birthMonth = birthMonth;
-            person.number = number*/;
-
-            //return person;
-
-            return new Person(name, age, birthMonth, number);
-        } 
-
-
+        }
 
         
-
-
-        /*static void ReturnPerson(ref string name , ref int age, ref int birthMonth)
-        {
-            Console.Write("Enter your name: ");
-            name = Console.ReadLine();
-
-            Console.Write("Enter your age: ");
-            age = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Enter your birth month: ");
-            birthMonth = Convert.ToInt32(Console.ReadLine());
-
-
-
-
-        }*/
 
 
                     
