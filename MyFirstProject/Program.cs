68748bfa31fa12b32504db4c457f6ec540cf1c39
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-09-04 16:37:19 +0800

    Class variable/function scopes

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -24,80 +24,67 @@ namespace MyFirstProject
         {
             private string name;
             private int age;
+            private string test = "hello";// global scope
             
 
             public Person(string name, int age) 
             {
                 this.name = name;
                 this.age = age;
-                
+                string test = "hi";// local scope and it takes priotity over global scope
+                Console.WriteLine(test);
+
             }
 
             public void SetName(string name)
             {
-                /*if (!string.IsNullOrEmpty(name))
-                {
-                this.name = name;
-                }
-                else
-                {
-                    this.name = "Invalid name";
-                }*/
-
+          
                 this.name = !string.IsNullOrEmpty(name) ? name : "Invalid name";
             }
 
-            /*public string GetName()
+          
+
+            public string GetName()
             {
                 return name;
-            }*/
+            }
 
-            public string GetName() => name;
+
+            public void SetAge(int age)
+            {
+                this.age = age >= 0 && age <= 150 ? age : -1;
+            }
+          
+
+            public int GetAge()
+            {
+               return age; 
+            }
 
             public string ReturnDetails()
             {
                 return $"Name: {name}\nAge: {age}";
             }
 
-            public void SetAge(int age)
-            {
-                /*if (age >= 0 && age <= 150)
-                {
-                    this.age = age;
-                }
-                else
-                {
-                    this.age = -1;
-                }*/
-                //condition ? true : false
-                this.age = age >= 0 && age <= 150 ? age : -1;
-            }
-
-            /*public int GetAge()
-            {
-                return age;
-            }*/
-
-            public int GetAge() => age;
-
         }
+
+        static int x = 5;
 
         static void Main(string[] args)
         {
+            
+
             Person person = new Person("AL", 23);            
             Console.WriteLine(person.ReturnDetails());
 
-            //person.name = "Art";
-            //person.age = 21;
-
-            person.SetName("Art");
-            person.SetAge(21);
-            Console.WriteLine(person.ReturnDetails());
-
-            Console.WriteLine($"Your name is {person.GetName()} amd your age is {person.GetAge()}");
-            
+            x = 20;
 
             Console.ReadLine();
+        }
+
+        static void something(Person person)
+        {
+            x = 10;
         }
 
         
